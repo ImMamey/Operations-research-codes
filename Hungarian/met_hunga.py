9066a2fcf8366b5a8dfa43580eaefbe6632a44ad
ImMamey <32584037+ImMamey@users.noreply.github.com>
2024-07-16 18:27:15 -0400

    Fix: conflicted merge
    Co-authored-by: Fussita

--- Hungarian/met_hunga.py
+++ Hungarian/met_hunga.py
@@ -6,15 +6,15 @@ from deprecated import deprecated
 
 
 @deprecated(reason="Usar create_and_print_df en vez de este metodo.")
-def mostrarMatriz(mt: list[int | Any], nfilas: int, ncolumnas: int) -> None:
-    # TODO: metodo deprecated, borrarlo una vez que se haya probado el nuevo metodo.
-    print("\n")
+def mostrarMatriz(mt: list[int | Any], nfilas:int, ncolumnas:int)->None:
+    #TODO: metodo deprecated, borrarlo una vez que se haya probado el nuevo metodo.
+    print('\n')
     for i in range(nfilas):
-        st = ""
+        st = ''
         for j in range(ncolumnas):
-            st = st + str(mt[i][j]) + "   "
+            st = st + str(mt[i][j]) + '   '
         print(st)
-    print("\n\n════════════════════════════════\n")
+    print('\n\n════════════════════════════════\n')
 
 
 def create_and_print_df(data: list[int | Any]) -> None:
@@ -37,9 +37,7 @@ def create_and_print_df(data: list[int | Any]) -> None:
     print(tabulate(df, headers="keys", tablefmt="double_grid", showindex=True))
 
 
-def crearMatriz(
-    mt: list[int | Any], nfilas: int, ncolumnas: int
-) -> list[list[int | Any]]:
+def crearMatriz(mt:list[int | Any], nfilas:int, ncolumnas:int)->list[list[int | Any]]:
     """
     Crea una matriz a partir de una lista cualquiera original, usando append.
     :param mt: Matriz original
@@ -56,7 +54,7 @@ def crearMatriz(
     return matriz
 
 
-def obtenerMenorFila(mt, nfila, ncolumnas) -> list[int | Any]:
+def obtenerMenorFila(mt, nfila, ncolumnas)->list[int | Any]:
     """
     Obtiene el menor valor de una fila en especifico
     :param mt: Matriz
@@ -65,15 +63,13 @@ def obtenerMenorFila(mt, nfila, ncolumnas) -> list[int | Any]:
     :return: Matriz
     """
     ax = mt[nfila][0]
-
     for j in range(ncolumnas):
-        if ax > mt[nfila][j]:
-
+        if (ax > mt[nfila][j]):
             ax = mt[nfila][j]
     return ax
 
 
-def obtenerMenorColumna(mt, nfilas, ncolumna) -> list[int | Any]:
+def obtenerMenorColumna(mt, nfilas, ncolumna)->list[int | Any]:
     """
     Obtiene el menor valor de una columna en especifico
     :param mt: Matriz original
@@ -83,7 +79,7 @@ def obtenerMenorColumna(mt, nfilas, ncolumna) -> list[int | Any]:
     """
     ax = mt[0][ncolumna]
     for j in range(nfilas):
-        if ax > mt[j][ncolumna]:
+        if (ax > mt[j][ncolumna]):
             ax = mt[j][ncolumna]
     return ax
 
@@ -93,12 +89,11 @@ def pasoDos(mat, nfilas, ncolumnas):
     ax = []
 
     # 2.1 Restar el menor de cada fila a cada elemento de la fila
-    print("2.1. Restar menor de Fila a Cada Elemento de la Fila")
+    print('2.1. Restar menor de Fila a Cada Elemento de la Fila')
     for i in range(nfilas):
         ax.append(obtenerMenorFila(mt, i, ncolumnas))
-        
-    print('2.1.1. Menor Valor a Restar por Fila')
 
+    print('2.1.1. Menor Valor a Restar por Fila')
     print(ax)
     for i in range(len(ax)):
         for j in range(ncolumnas):
@@ -106,20 +101,20 @@ def pasoDos(mat, nfilas, ncolumnas):
     create_and_print_df(mt)
 
     # 2.2 Restar el menor de cada columna a cada elemento de la columna
-    print("2.2. Restar menor de Columna a Cada Elemento de la Columna")
+    print('2.2. Restar menor de Columna a Cada Elemento de la Columna')
     bx = []
     for i in range(ncolumnas):
         bx.append(obtenerMenorColumna(mt, nfilas, i))
-    print("\n2.2.1. Valores Menores a Restar por Columna")
+    print('\n2.2.1. Valores Menores a Restar por Columna')
     print(bx)
     for i in range(len(bx)):
         for j in range(nfilas):
             mt[j][i] -= bx[i]
     create_and_print_df(mt)
-    
+
     conf = revisarAsignaciones(mt, nfilas, ncolumnas)
 
-    if conf == True:
+    if (conf == True):
         return mt
     else:
         mtb = []
@@ -129,23 +124,23 @@ def pasoDos(mat, nfilas, ncolumnas):
                 ax.append(mt[i][j])
             mtb.append(ax)
 
-        print("3.1. Mínimo número de Asignación de Líneas \n")
+        print('3.1. Mínimo número de Asignación de Líneas \n')
         nano = nfilas
         cont = 1
 
         interFilas = []
         interColumnas = []
-        while nano > 0:
+        while (nano > 0):
             for i in range(nfilas):
-                if mtb[i].count(0) == nano:
+                if (mtb[i].count(0) == nano):
                     killFila(mtb, ncolumnas, i)
-                    print("Numero de Asignación: " + str(cont))
+                    print('Numero de Asignación: ' + str(cont))
                     cont += 1
                     interFilas.append(i)
                     create_and_print_df(mtb)
-                if cerosColumnas(mtb, nfilas, i) == nano:
+                if (cerosColumnas(mtb, nfilas, i) == nano):
                     killColumn(mtb, nfilas, i)
-                    print("Numero de Asignación: " + str(cont))
+                    print('Numero de Asignación: ' + str(cont))
                     cont += 1
                     interColumnas.append(i)
                     create_and_print_df(mtb)
@@ -154,15 +149,15 @@ def pasoDos(mat, nfilas, ncolumnas):
         minVal = 99999999
         for i in range(nfilas):
             for j in range(ncolumnas):
-                if type(mtb[i][j]) == type(3):
-                    if mtb[i][j] < minVal:
+                if (type(mtb[i][j]) == type(3)):
+                    if (mtb[i][j] < minVal):
                         minVal = mtb[i][j]
 
-        print("3.2. Mínimo valor de la tabla Resultante: " + str(minVal))
-        print("3.2.1. Tabla Resultante de la Resta del Mínimo Valor")
+        print('3.2. Mínimo valor de la tabla Resultante: ' + str(minVal))
+        print('3.2.1. Tabla Resultante de la Resta del Mínimo Valor')
         for i in range(nfilas):
             for j in range(ncolumnas):
-                if type(mtb[i][j]) == type(3):
+                if (type(mtb[i][j]) == type(3)):
                     mtb[i][j] -= minVal
 
         for i in interFilas:
@@ -171,32 +166,31 @@ def pasoDos(mat, nfilas, ncolumnas):
 
         create_and_print_df(mtb)
 
-        print("Tabla obtenida en el Paso 2")
+        print('Tabla obtenida en el Paso 2')
 
         create_and_print_df(mt)
 
         for i in range(nfilas):
             for j in range(ncolumnas):
-                if type(mtb[i][j]) == type(3):
+                if (type(mtb[i][j]) == type(3)):
                     mt[i][j] = mtb[i][j]
-    
-        print('3.2.2. Combinación de Ambas Tablas')
 
+        print('3.2.2. Combinación de Ambas Tablas')
         create_and_print_df(mt)
         conf = revisarAsignaciones(mt, nfilas, ncolumnas)
 
-        if conf == True:
+        if (conf == True):
             return mt
         else:
             #print('Hay que ciclar Compai')
             return pasoDos(mt, nfilas, ncolumnas)
-            
+
 
 
 def cerosColumnas(mt, nfilas, ncolu):
     cont = 0
     for i in range(nfilas):
-        if mt[i][ncolu] == 0:
+        if (mt[i][ncolu] == 0):
             cont += 1
     return cont
 
@@ -206,31 +200,29 @@ def revisarAsignaciones(matriz, nfilas, ncolumnas):
     coors = []
     nano = 1
     cond = 1
-    
+
     for i in range(nfilas):
         ax = []
         for j in range(ncolumnas):
             ax.append(matriz[i][j])
         mt.append(ax)
 
-    while True:
+    while (True):
         cond = 0
         for i in range(nfilas):
-
-            if mt[i].count(0) == nano:
+            if (mt[i].count(0) == nano): # Reviso si Nano = Cantidad de Ceros en la Fila
                 cond = 1
                 for j in range(ncolumnas):
-                    if mt[i][j] == 0:
+                    if (mt[i][j] == 0):
                         killColumn(mt, nfilas, j)
                         killFila(mt, ncolumnas, i)
                         coors.append([i + 1, j + 1])
                     else:
-                        mt[i][j] = "-"
-        if cond == 0:
+                        mt[i][j] = '-'
+        if (cond == 0):
             nano += 1
-        if nano > ncolumnas:
+        if (nano > ncolumnas):
             break
-
 
     # LA RESUELVE TODO 44 MIL
     for x in range(5):
@@ -241,20 +233,19 @@ def revisarAsignaciones(matriz, nfilas, ncolumnas):
                     killFila(mt, ncolumnas, i)
                     coors.append([i + 1, j + 1])
                     mt[i][j] = '-'
-    
-    print('2.3. Asignación de Líneas')
 
+    print('2.3. Asignación de Líneas')
     print(coors)
     create_and_print_df(mt)
 
-    if ncolumnas == len(coors):
-        print("Solucion Optima:")
+    if (ncolumnas == len(coors)):
+        print('Solucion Optima:')
         print(coors)
         return True
     else:
-        print("No se obtuvo una solución optima, se procede al Paso 3")
+        print('No se obtuvo una solución optima, se procede al Paso 3')
         print(coors)
-        print("\n\n════════════════════════════════\n")
+        print('\n\n════════════════════════════════\n')
         return False
 
 def obtenerCoors(matriz, nfilas, ncolumnas):
@@ -269,21 +260,21 @@ def obtenerCoors(matriz, nfilas, ncolumnas):
             ax.append(matriz[i][j])
         mt.append(ax)
 
-    while True:
+    while (True):
         cond = 0
         for i in range(nfilas):
-            if mt[i].count(0) == nano:
+            if (mt[i].count(0) == nano):
                 cond = 1
                 for j in range(ncolumnas):
-                    if mt[i][j] == 0:
+                    if (mt[i][j] == 0):
                         killColumn(mt, nfilas, j)
                         killFila(mt, ncolumnas, i)
                         coors.append([i + 1, j + 1])
                     else:
-                        mt[i][j] = "-"
-        if cond == 0:
+                        mt[i][j] = '-'
+        if (cond == 0):
             nano += 1
-        if nano > ncolumnas:
+        if (nano > ncolumnas):
             break
 
     # LA RESUELVE TODO 44 MIL
@@ -295,18 +286,18 @@ def obtenerCoors(matriz, nfilas, ncolumnas):
                     killFila(mt, ncolumnas, i)
                     coors.append([i + 1, j + 1])
                     mt[i][j] = '-'
-        
+
     return coors
 
 
 def killFila(mt, ncolumnas, nfila):
     for i in range(ncolumnas):
-        mt[nfila][i] = "-"
+        mt[nfila][i] = '-'
 
 
 def killColumn(mt, nfilas, ncolumn):
     for i in range(nfilas):
-        mt[i][ncolumn] = "|"
+        mt[i][ncolumn] = '|'
 
 
 def crearCeros(nCant):
@@ -317,29 +308,31 @@ def crearCeros(nCant):
 
 
 def pasoUno(mt, nfilas, ncolumnas):
-    if ncolumnas > nfilas:
+    if (ncolumnas > nfilas):
         diff = ncolumnas - nfilas
         aux = crearCeros(ncolumnas)
 
         for i in range(ncolumnas - nfilas):
             mt.append(aux)
 
-    if ncolumnas < nfilas:
+    if (ncolumnas < nfilas):
         for j in range(nfilas - ncolumnas):
             for i in range(nfilas):
                 mt[i].append(0)
 
 
-def inicio(rest, f_o):
+
+def inicio(rest,f_o):
+
     mtBalanceada = crearMatriz(rest, len(rest), len(rest[0]))
     # TODO: verificar usos de estos dos, si solo se usa para imprimir, se puede borrar.
     nfilas = len(mtBalanceada)
     ncolumnas = len(mtBalanceada[0])
 
-    print("Tabla Inicial")
+    print('Tabla Inicial')
     create_and_print_df(mtBalanceada)
 
-    if f_o == "min":
+    if (f_o == 'min'):
         for i in range(nfilas):
             for j in range(ncolumnas):
                 mtBalanceada[i][j] = mtBalanceada[i][j] * (-1)
@@ -349,14 +342,14 @@ def inicio(rest, f_o):
     nfilas = len(mtBalanceada)
     ncolumnas = len(mtBalanceada[0])
 
-    print("Tabla Balanceada")
+    print('Tabla Balanceada')
     create_and_print_df(mtBalanceada)
     mt = pasoDos(mtBalanceada, nfilas, ncolumnas)
     coors = obtenerCoors(mt, nfilas, ncolumnas)
 
     mtCoors = crearMatriz(rest, len(rest), len(rest[0]))
     pasoUno(mtCoors, len(rest), len(rest[0]))
-    costo = 0
+    costo = 0;
     costos = []
 
     for k in coors:
@@ -364,18 +357,18 @@ def inicio(rest, f_o):
         b = k[1] - 1
         costo += mtCoors[a][b]
         costos.append(mtCoors[a][b])
-        print("> El " + str(a + 1) + " ocupa el " + str(b + 1))
+        print('> El ' + str(a + 1) + ' ocupa el ' + str(b + 1))
 
-    print("Costos: ")
+    print('Costos: ')
     print(costos)
-    print("Costo Total: " + str(costo))
+    print('Costo Total: ' + str(costo))
 
     return coors, costo
 
 
 if (__name__ == '__main__'):
 
-    # Ejercicio N-1 
+    # Ejercicio N-1
     """ rest = [
         [50, 130, 190],
         [130, 100, 150],
@@ -391,5 +384,73 @@ if (__name__ == '__main__'):
         [2, 3, 4, 3, 2],
         [7, 6, 8, 6, 7],
         [5, 4, 4, 6, 3]
+    ]
+    f_o = 'min' """
+
+    # Ejercicio N-3
+    """ rest = [
+        [32, 19, 29],
+        [26, 25, 20],
+        [36, 28, 26]
+    ]
+    f_o = 'max' """
+
+    # Ejercicio N-4 (Ciclico)
+    """ rest = [
+        [32, 19, 29, 8],
+        [26, 25, 20, 3],
+        [36, 28, 26, 3],
+        [39, 22, 32, 8]
+    ]
+    f_o = 'max' """
+
+    # Ejercicio N-5
+    """ rest = [
+        [10, 9, 5],
+        [9, 8, 3],
+        [6, 4, 7]
+    ]
+    f_o = 'max' """
+
+    # Ejercicio N-6 (ERROR)
+    """ rest = [
+        [3, 5, 3, 3],
+        [5, 14, 10, 10],
+        [12, 6, 19, 17],
+        [2, 17, 10, 12]
+    ]
+    f_o = 'max' """
+
+    # Ejercicio N-7
+    """ rest = [
+        [180, 150, 200, 200],
+        [250, 305, 450, 500],
+        [200, 208, 320, 100]
+    ]
+    f_o = 'max' """
+
+    # Ejercicio N-8
+    """ rest = [
+        [11800, 15000, 20000],
+        [12500, 13000, 14400],
+        [20000, 18000, 23000],
+        [18000, 17000, 16000]
+    ]
+    f_o = 'max' """
+
+    # Ejercicio N-9 (MIN)
+    rest = [
+        [13, 7, 12, 12],
+        [10, 13, 15, 7],
+        [13, 10, 8, 8]
+    ]
+    f_o = 'min'
+
     inicio(rest, f_o)
+
+
+
+
+
+
 
